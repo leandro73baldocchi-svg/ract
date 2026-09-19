a5c5946cba9db49b10a154bf2111c642a1402313
leandro73baldocchi-svg <leandro73baldocchi@gmail.com>
2026-09-19 11:01:33 -0300

    Create firebase.ts

---
+++ src/utils/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD23vUYTXGLp4kq9NWYfKsQVGkUoFue7bc",
+  authDomain: "ract-31120.firebaseapp.com",
+  projectId: "ract-31120",
+  storageBucket: "ract-31120.firebasestorage.app",
+  messagingSenderId: "164124157182",
+  appId: "1:164124157182:web:f008438b66da3233fa31af"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
